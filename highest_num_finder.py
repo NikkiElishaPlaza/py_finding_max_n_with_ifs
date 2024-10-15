2f4e4b2528cb41a1a7e63091760e5dcb7c2d4d82
Nikki Elisha Plaza <nikkiplaza24@gmail.com>
2024-10-15 22:33:21 +0800

    copy pasted the first code to four lines and changed its variable + variable name

--- highest_num_finder.py
+++ highest_num_finder.py
@@ -3,7 +3,10 @@ def get_highest_num_from_user_input():
 
 #Getting user to input five (5) numbers
     number_1 = int(float(input("Kindly enter your first number: ")))
-
+    number_2 = int(float(input("Kindly enter your second number: ")))
+    number_3 = int(float(input("Kindly enter your third number: ")))
+    number_4 = int(float(input("Kindly enter your fourth number: ")))
+    number_5 = int(float(input("Kindly enter your fifth number: ")))
 
 #Find and print the highest number using only if statement
 
