13c48947bcb503eb82e6e293dcf82d44e0e69335
Nikki Elisha Plaza <nikkiplaza24@gmail.com>
2024-10-15 22:29:00 +0800

    added how user will input number. (made it a float also if the user want to add decimal)

--- highest_num_finder.py
+++ highest_num_finder.py
@@ -1,9 +1,14 @@
 #Adding function to find the highest number from a user
+def get_highest_num_from_user_input():
 
 #Getting user to input five (5) numbers
-
+    number_1 = int(float(input("Kindly enter your first number: ")))
 
 
 #Find and print the highest number using only if statement
 
 #Finalize the code
+
+
+#Call the function
+get_highest_num_from_user_input()
