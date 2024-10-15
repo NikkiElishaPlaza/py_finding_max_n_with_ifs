ff3080cea4418335b93a516196252bd16e2258ce
Nikki Elisha Plaza <nikkiplaza24@gmail.com>
2024-10-15 23:34:16 +0800

    fixed code. removed int

--- highest_num_finder.py
+++ highest_num_finder.py
@@ -2,11 +2,11 @@
 def get_highest_num_from_user_input():
 
 #Getting user to input five (5) numbers
-    number_1 = int(float(input("Kindly enter your first number: ")))
-    number_2 = int(float(input("Kindly enter your second number: ")))
-    number_3 = int(float(input("Kindly enter your third number: ")))
-    number_4 = int(float(input("Kindly enter your fourth number: ")))
-    number_5 = int(float(input("Kindly enter your fifth number: ")))
+    number_1 = float(input("Kindly enter your first number: "))
+    number_2 = float(input("Kindly enter your second number: "))
+    number_3 = float(input("Kindly enter your third number: "))
+    number_4 = float(input("Kindly enter your fourth number: "))
+    number_5 = float(input("Kindly enter your fifth number: "))
 
 #Assuming that number_1 is the highest
     highest = number_1
